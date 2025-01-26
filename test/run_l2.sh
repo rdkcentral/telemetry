0ef77c35f44facefc751e73cfa828a252ebc919b
yogeswaransky <166126056+yogeswaransky@users.noreply.github.com>
2025-01-26 22:35:43 +0530

    RDK-49250: Update run_l2.sh

--- test/run_l2.sh
+++ test/run_l2.sh
@@ -25,3 +25,4 @@ mkdir -p "$RESULT_DIR"
 #pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/runs_as_daemon.json test/functional-tests/tests/test_runs_as_daemon.py
 #pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/bootup_sequence.json test/functional-tests/tests/test_bootup_sequence.py
 #pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/xconf_communications.json test/functional-tests/tests/test_xconf_communications.py
+pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/msg_packet.json test/functional-tests/tests/test_multiprofile_msgpacket.py

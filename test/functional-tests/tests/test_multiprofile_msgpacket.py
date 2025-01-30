c7406930276c72894d7b1133611524073fe6795c
Yogeswaran K <166126056+yogeswaransky@users.noreply.github.com>
2025-01-31 02:10:49 +0530

    RDK-49250: Update test_multiprofile_msgpacket.py

--- test/functional-tests/tests/test_multiprofile_msgpacket.py
+++ test/functional-tests/tests/test_multiprofile_msgpacket.py
@@ -49,7 +49,7 @@ def tomsgpack(json_string):
     print(base64_data)
     return base64_data
 
-#negative case without name field
+#negative case without name field, Empty string in namefield & without hash field
 @pytest.mark.run(order=1)
 def test_without_namefield():
     clear_T2logs()
@@ -72,32 +72,39 @@ def test_without_namefield():
     assert HASH_ERROR_MSG in grep_T2logs(HASH_ERROR_MSG) #without hash field
     sleep(10)
 
-#negative case without hashvalue
+#negative case without hashvalue, without version field & without Protocol field
 @pytest.mark.run(order=2)
 def test_without_hashvalue():
     clear_T2logs()
     rbus_set_data(T2_REPORT_PROFILE_PARAM_MSG_PCK, "string", tomsgpack(data_without_hashvalue))
     sleep(20)
-    assert "TR_AC12" in grep_T2logs(LOG_PROFILE_ENABLE) #  without hash value
-    assert "TR_AC14" in grep_T2logs(LOG_PROFILE_ENABLE) #  without version field
-    assert "TR_AC15" not in grep_T2logs(LOG_PROFILE_ENABLE) #  without Protocol field
+    assert "TR_AC12" in grep_T2logs(LOG_PROFILE_ENABLE)  # without hash value
+    assert "TR_AC14" in grep_T2logs(LOG_PROFILE_ENABLE)  # without version field
+    assert "TR_AC15" not in grep_T2logs(LOG_PROFILE_ENABLE)  # without Protocol field
     sleep(10)
 
-#negative - random value for Protocol 
+#negative cases:
+# random value for Protocol 
+# empty string for version
+# empty string for protocol
 @pytest.mark.run(order=3)
 def test_with_wrong_protocol_value():
     clear_T2logs()
     rbus_set_data(T2_REPORT_PROFILE_PARAM_MSG_PCK, "string", tomsgpack(data_with_wrong_protocol_value))
     sleep(20)
     ERROR_WRONG_PROTOCOL = "Unsupported report sending protocol"
-    assert ERROR_WRONG_PROTOCOL in grep_T2logs(ERROR_WRONG_PROTOCOL)
-    assert "TR_AC16" not in grep_T2logs(LOG_PROFILE_ENABLE) #  
-    assert "TR_AC17" in grep_T2logs(LOG_PROFILE_ENABLE) #  To confirm for version
-    assert "TR_AC13" not in grep_T2logs(LOG_PROFILE_ENABLE) #  To confirm for Protocol
+    assert ERROR_WRONG_PROTOCOL in grep_T2logs(ERROR_WRONG_PROTOCOL) #Verify the right protocol is given
+    assert "TR_AC16" not in grep_T2logs(LOG_PROFILE_ENABLE) # Verify profile is not enabled with an incorrect protocol
+    assert "TR_AC17" in grep_T2logs(LOG_PROFILE_ENABLE) # Verify Profile can be enabled for empty version
+    assert "TR_AC13" not in grep_T2logs(LOG_PROFILE_ENABLE) # Verify Profile cannot be enabled for empty protocol
     sleep(5)
 
-
-#negative case without EncodingType & ActivationTimeout values
+#negative cases
+# without EncodingType & ActivationTimeout values
+# without encodingType param
+# without ActivationTimeout param
+# without ReportingInterval param
+# without GenerateNow param
 @pytest.mark.run(order=4)
 def test_without_EncodingType_ActivationTimeout_values():
     ERROR_REPORTING_INTERVAL = "If TriggerCondition is not given ReportingInterval parameter is mandatory"
@@ -111,18 +118,18 @@ def test_without_EncodingType_ActivationTimeout_values():
     rbus_set_data(T2_REPORT_PROFILE_PARAM_MSG_PCK, "string", tomsgpack(data_without_EncodingType_ActivationTimeout_values))
     sleep(20)
     sleep(5)
-    assert "TR_AC18" in grep_T2logs(LOG_PROFILE_ENABLE) #  To confirm 
-    assert "TR_AC19" in grep_T2logs(LOG_PROFILE_ENABLE) #  To confirm 
-    assert "TR_AC20" not in grep_T2logs(LOG_PROFILE_ENABLE) #  To confirm 
-    assert "TR_AC21" in grep_T2logs(LOG_PROFILE_ENABLE) #  To confirm
-    assert ERROR_REPORTING_INTERVAL in grep_T2logs(ERROR_REPORTING_INTERVAL)
-    assert "TR_AC22" not in grep_T2logs(LOG_PROFILE_ENABLE) #  To confirm 
-    assert "TR_AC23" in grep_T2logs(LOG_PROFILE_ENABLE) #  To confirm
+    assert "TR_AC18" in grep_T2logs(LOG_PROFILE_ENABLE) # Verify profile is enabled with an empty encodingType
+    assert "TR_AC19" in grep_T2logs(LOG_PROFILE_ENABLE) # Verify profile is enabled with an empty ActivationTimeout
+    assert "TR_AC20" not in grep_T2logs(LOG_PROFILE_ENABLE) # Verify profile is not enabled without encodingType param
+    assert "TR_AC21" in grep_T2logs(LOG_PROFILE_ENABLE) # Verify profile is enabled without ActivationTimeout param
+    assert ERROR_REPORTING_INTERVAL in grep_T2logs(ERROR_REPORTING_INTERVAL) # Verify ReportingInterval error is thrown
+    assert "TR_AC22" not in grep_T2logs(LOG_PROFILE_ENABLE) # Verify profile is not enabled without ReportingInterval param
+    assert "TR_AC23" in grep_T2logs(LOG_PROFILE_ENABLE) # Verify profile is enabled without GenerateNow param
     sleep(5)
 
-
 #1).positive case for working of Reporting Interval
-#2).positive case for event marker & count
+#2).positive case for event marker & with count
+#3).positive case for event marker with accumulate
 @pytest.mark.run(order=5)
 def test_reporting_interval_working():
     clear_T2logs()
@@ -162,7 +169,7 @@ def test_reporting_interval_working():
 # Trim - grep marker validation
 # Datamodel validation
 @pytest.mark.run(order=6)
-def test_for_activation_timeout():
+def test_for_Generate_Now():
     os.makedirs('/opt/logs', exist_ok=True)
 	# Create log file with the logs needed for grep marker
     file = open('/opt/logs/core_log.txt', 'w')
@@ -179,7 +186,7 @@ def test_for_activation_timeout():
     file.close()
 
     LOG_GENERATE_NOW = "Waiting for 0 sec for next TIMEOUT for profile"
-    rbus_set_data(T2_REPORT_PROFILE_PARAM_MSG_PCK, "string", tomsgpack(data_with_activation_timeout))
+    rbus_set_data(T2_REPORT_PROFILE_PARAM_MSG_PCK, "string", tomsgpack(data_with_Generate_Now))
     sleep(20)
     assert "TR_AC777" in grep_T2logs(LOG_GENERATE_NOW)  # verification for GenerateNow
     sleep(10)
@@ -187,10 +194,12 @@ def test_for_activation_timeout():
     assert "FILE_Upload_Progress\":\" newfile1 20%" in grep_T2logs("cJSON Report ") #  absolute - grep marker validation
     assert "FILE_Read_Progress\":\"newfile2 line 10" in grep_T2logs("cJSON Report ") #  Trim - grep marker validation
     assert "MODEL_NAME" in grep_T2logs("cJSON Report ") #  Datamodel validation
-    #assert "USED_MEM1_split" in grep_T2logs("cJSON Report ") #  TO be Uncommented once reportEmpty is fixed
     sleep(10)
 
-#Negative case with activation timeout less than reporting interval
+# Negative case with activation timeout less than reporting interval
+# Postive case for Empty report sent when reportEmpty is true
+# Positive case for FirstReporting Interval 
+# DCM profile is running parallel to the Multiprofile - positve
 @pytest.mark.run(order=7)
 def test_for_invalid_activation_timeout():
     ERROR_PROFILE_TIMEOUT = "activationTimeoutPeriod is less than reporting interval. invalid profile: "
@@ -205,17 +214,20 @@ def test_for_invalid_activation_timeout():
 
     rbus_set_data(T2_REPORT_PROFILE_PARAM_MSG_PCK, "string", tomsgpack(data_with_less_activation_timeout))
     sleep(60)
-    assert "TR_AC88" in grep_T2logs(ERROR_PROFILE_TIMEOUT)
-    assert "MODEL_NAME\":\"NULL" in grep_T2logs("cJSON Report ") #verify Empty report
-    assert "TR_AC6919" in grep_T2logs("firstreporting interval is given")
-    assert "5 sec" in grep_T2logs("firstreporting interval is given")
+    assert "TR_AC88" in grep_T2logs(ERROR_PROFILE_TIMEOUT) # Verify profile not set if activation timeout is less than reporting interval
+    assert "MODEL_NAME\":\"NULL" in grep_T2logs("cJSON Report ") # verify Empty report is sent for reportEmpty is true
+    assert "TR_AC6919" in grep_T2logs("firstreporting interval is given") #
+    assert "5 sec" in grep_T2logs("firstreporting interval is given") #} Verify Firstreporting Interval is working
     assert "NEW TEST PROFILE" in grep_T2logs(LOG_PROFILE_SET) # Verify DCM profile is set
     assert "60 sec" in grep_T2logs("reporting interval is taken - NEW TEST PROFILE") #Verify DCM profile is running
     assert "AccountId\":\"Platform_Container_Test_DEVICE" in grep_T2logs("cJSON Report ") #verify report generated for DCM profile
     sleep(10)
 
 #1).positive case for activation timeout
-#2).positive case with delete on timeout
+#2).regex - grep marker validation
+#3).regex - Datamodel validation
+#4).regex - Event marker validation
+#5).positive case with delete on timeout
 @pytest.mark.run(order=8)
 def test_with_delete_on_timeout():
     clear_T2logs()
@@ -236,6 +248,5 @@ def test_with_delete_on_timeout():
     assert "MODEL_NAME\":\"DOCKER" in grep_T2logs("cJSON Report ") #  regex - Datamodel validation
     assert "TEST_EVENT_MARKER_2\":\"17" in grep_T2logs("cJSON Report ") #  regex - Event marker validation 
     #assert ""  To be updated once the DeleteOnTimeout is fixed and a LOG is added.
-    #cJSON Report = {"FR2_US_TC3":[{"MODEL_NAME":"DOCKER"},{"SYS_INFO_CrashPortalUpload_success":"200"},{"TEST_EVENT_MARKER_2":"17"}]}
 
     sleep(5)

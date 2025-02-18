2be55e5ed065a646e6e12ec02fcf6a3e9219f362
Yogeswaran K <166126056+yogeswaransky@users.noreply.github.com>
2025-02-18 17:01:08 +0530

    RDK-55755: [RDK-E] Optimize Telemetry 2.0 Client Library

--- source/bulkdata/t2eventreceiver.c
+++ source/bulkdata/t2eventreceiver.c
@@ -213,7 +213,8 @@ T2ERROR T2ER_Init()
     }
 
     system("touch /tmp/.t2ReadyToReceiveEvents");
-    setRbusParamValue(T2_COMPONENT_READY);
+    setT2EventReceiveState(T2_STATE_COMPONENT_READY);
+    T2Info("T2 is now Ready to Recieve Events\n");
 
     T2Debug("%s --out\n", __FUNCTION__);
     return T2ERROR_SUCCESS;

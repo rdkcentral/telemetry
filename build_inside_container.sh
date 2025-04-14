e254782552920f06e1f8da2e1d1ee761decf3220
shibu-kv <shibu.kakkoth@gmail.com>
2025-04-14 08:27:48 -0700

    Coredump support

--- build_inside_container.sh
+++ build_inside_container.sh
@@ -24,11 +24,29 @@ export top_builddir=`pwd`
 
 autoreconf --install
 
+INPUT=$1
+
 # FLags to print compiler warnings
-DEBUG_CFLAGS="-Wall -Werror -Wextra"
+DEBUG_CFLAGS="-Wall -Werror -Wextra" 
+
+if [ "$INPUT" = "tsan" ]; then
+    DEBUG_CFLAGS="${DEBUG_CFLAGS} -g -O0 -fsanitize=thread"
+    echo "Thread sanitizer enabled"
+    echo "LD_PRELOAD=$(gcc -print-file-name=libtsan.so)"
+elif [ "$INPUT" = "asan" ]; then
+    DEBUG_CFLAGS="${DEBUG_CFLAGS} -g -O0 -fsanitize=address"
+    echo "Address sanitizer enabled"
+    echo "LD_PRELOAD=$(gcc -print-file-name=libasan.so)"
+else
+   echo "No additional flags provided"
+fi
+
+# -fsanitize=address -fsanitize=thread 
 
 export CFLAGS=" ${DEBUG_CFLAGS} -I${INSTALL_DIR}/include/rtmessage -I${INSTALL_DIR}/include/msgpack -I${INSTALL_DIR}/include/rbus -I${INSTALL_DIR}/include -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/local/include -DFEATURE_SUPPORT_WEBCONFIG -DRDK_LOGGER" 
 
 export LDFLAGS="-L/usr/lib/x86_64-linux-gnu -lglib-2.0"
+# LD_PRELOAD for address sanitizer
+# export LD_PRELOAD=$(gcc -print-file-name=libasan.so)
 
 ./configure --prefix=${INSTALL_DIR} && make && make install

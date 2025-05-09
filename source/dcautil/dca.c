0de3b3b0626ba753064c4b4f6d7c8db865db7b0c
Yogeswaran K <166126056+yogeswaransky@users.noreply.github.com>
2025-05-09 14:50:31 +0000

    RDK-56123: [RDKE] Fix coverity issues in Telemetry

--- source/dcautil/dca.c
+++ source/dcautil/dca.c
@@ -160,8 +160,9 @@ static void appendData(pcdata_t* dst, const char* src)
         dst->data = (char*) realloc(dst->data, dst_len + src_len);
         if(NULL != dst->data)
         {
-            strncat(dst->data, ",", 1);
-            snprintf((dst->data) + dst_len - 1, src_len, "%s", src);
+            dst->data[dst_len - 1] = ',';
+            dst->data[dst_len] = '\0'; 
+            snprintf((dst->data) + dst_len, src_len, "%s", src);
         }
         else
         {

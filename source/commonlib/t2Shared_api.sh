f417d9b038cffe2c451e4713f3af40c29479f6fd
shivabhaskar <c.shivabhaskar97@gmail.com>
2025-05-16 14:55:41 +0530

    RDK-57777 : New Api t2FilterMapCountNotify in t2Shared_api.sh
    Reason for change: Provide an API to call markers with common name.
      replacing the need for an if-else ladder.
    Test Procedure: check if the events are sent to T2 with the common name
      from the sehell script are as expected.
    Risks: Low

--- source/commonlib/t2Shared_api.sh
+++ source/commonlib/t2Shared_api.sh
@@ -39,3 +39,27 @@ t2ValNotify() {
         $T2_MSG_CLIENT "$marker" "$*" &
     fi
 }
+
+# This API helps to send events to T2 without exposing the actual variable names used.
+# It is particularly useful when the final markers do not follow a common syntax.
+#
+# Before calling this function, the required variables must be initialized with the common names.
+# These common names can be created by sourcing from a file (without exposing in the code) or by defining the variables directly.
+# For example:
+#   T2_CRASH_telemetry2_0="SYS_ERR_T2_CRASH"
+#   T2_CRASH_rtrouted="SYST_INFO_RBUS_CRASH"
+#
+# After defining the common names, the function can be called as follows:
+#   appname="telemetry2_0"
+#   t2FilterMapCountNotify T2_CRASH_${appname}
+#
+# In this example, the function call will internally call t2CountNotify with the argument "SYS_ERR_T2_CRASH".
+
+
+t2FilterMapCountNotify(){
+
+    common_name="$1"
+    if [ -n "${!common_name-}" ]; then
+        t2CountNotify "${!common_name}"
+    fi
+}

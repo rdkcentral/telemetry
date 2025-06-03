222405fcb02caba0a6684be2da9caef2cd5cd461
Ananth916 <74174916+Ananth916@users.noreply.github.com>
2025-06-03 11:42:01 +0530

    Update curlinterface.c

--- source/protocol/http/curlinterface.c
+++ source/protocol/http/curlinterface.c
@@ -257,10 +257,7 @@ static T2ERROR setPayload(CURL *curl, const char* payload, childResponse *childC
 #ifdef LIBRDKCERTSEL_BUILD
 void checkStateRed(char *cert_buf, size_t buf_size)
 {
-    if (access("/tmp/stateRedEnabled", F_OK) == 0) 
-    {
-         T2Debug("%s Device is not in red state\n", __FUNCTION__);
-    }
+    T2Debug("%s Device is not in red state\n", __FUNCTION__);
 }
 void curlCertSelectorFree()
 {

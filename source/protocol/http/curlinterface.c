874b7ae0593760c35726cdd523cd8ad0b62d84cb
Ananth916 <74174916+Ananth916@users.noreply.github.com>
2025-06-11 12:38:37 +0530

    Update curlinterface.c

--- source/protocol/http/curlinterface.c
+++ source/protocol/http/curlinterface.c
@@ -330,12 +330,15 @@ T2ERROR sendReportOverHTTP(char *httpUrl, char *payload, pid_t* outForkedPid)
         curlCertSelectorInit();
     }
     state_red_enable = isStateRedEnabled();
+    T2Info("%s: state_red_enable: %d\n", __func__, state_red_enable );
     if (state_red_enable)
     {
+        T2Info("%s: Device is in state red: %d\n", __func__ );
         thisCertSel = curlRcvryCertSelector;
     } 
     else
     {
+        T2Info("%s: Device is NOT in state red: %d\n", __func__ );
         thisCertSel = curlCertSelector;
     }
 #endif

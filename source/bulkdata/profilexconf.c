6fdfce8389f7c9a8d83f79232671fbb3fe76c151
PriyaDharshini_Kathiravan <priyakathiravan05@gmail.com>
2025-06-24 03:55:54 +0000

    DELIA-67022:T2 crash with signature strcmp during reboot test
    Reason for change: To avoid the strcmp crash during reboot test
    Test Procedure: There should not be crash and regression issues
    Risks: Low
    Priority: P1

--- source/bulkdata/profilexconf.c
+++ source/bulkdata/profilexconf.c
@@ -639,7 +639,7 @@ bool ProfileXConf_isNameEqual(char* profileName)
     if(initialized)
     {
         T2Info("singleProfile->name = %s and profileName = %s\n", singleProfile->name, profileName);
-        if(singleProfile && (singleProfile->name != NULL) && (profileName != NULL) && !strcmp(singleProfile->name, profileName))
+        if(singleProfile && (singleProfile->name != NULL) && (profileName != NULL) && !strcmp(singleProfile->name, profileName)) //Adding NULL check to avoid strcmp crash
         {
             isName = true;
         }

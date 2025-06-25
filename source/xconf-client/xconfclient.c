d6b9209706f8b3e0ff493c832f1a64ab1f43d6d2
Yogeswaran <yogeswaransky@gmail.com>
2025-06-25 15:10:01 +0000

    Test change for L2 failure
    Signed-off-by: Yogeswaran <yogeswaransky@gmail.com>

--- source/xconf-client/xconfclient.c
+++ source/xconf-client/xconfclient.c
@@ -87,7 +87,7 @@ static pthread_cond_t xcThreadCond;
 static rdkcertselector_h xcCertSelector = NULL;
 #endif
 #if defined(ENABLE_REMOTE_PROFILE_DOWNLOAD)
-    static int retryCount = 0;
+static int retryCount = 0;
 #endif
 
 T2ERROR ReportProfiles_deleteProfileXConf(ProfileXConf *profile);
@@ -944,7 +944,8 @@ status_return :
 }
 
 #if defined(ENABLE_REMOTE_PROFILE_DOWNLOAD)
-static char* getReportProfileConfigUrl() {
+static char* getReportProfileConfigUrl()
+{
     T2Debug("%s ++in\n", __FUNCTION__);
     FILE *fp = NULL;
     char buf[256] = {0};
@@ -954,17 +955,21 @@ static char* getReportProfileConfigUrl() {
     if (fp)
     {
         fgets(buf, sizeof(buf), fp);
-        T2Debug("%s buf::%s\n", __FUNCTION__,buf);
+        T2Debug("%s buf::%s\n", __FUNCTION__, buf);
         /*we need to remove the \n char in buf*/
-        if ((p = strchr(buf, '\n'))) *p = 0;
-        url=strdup(buf);
+        if ((p = strchr(buf, '\n')))
+        {
+            *p = 0;
+        }
+        url = strdup(buf);
         pclose(fp);
     }
     T2Debug("%s ++out\n", __FUNCTION__);
     return url;
 }
 
-static T2ERROR fetchRemoteReportProfileConfiguration(char **configData) {
+static T2ERROR fetchRemoteReportProfileConfiguration(char **configData)
+{
     T2Debug("%s ++in\n", __FUNCTION__);
     T2ERROR ret = T2ERROR_FAILURE;
 
@@ -972,8 +977,8 @@ static T2ERROR fetchRemoteReportProfileConfiguration(char **configData) {
 
     char* urlWithParams = (char*) malloc(MAX_URL_LEN * sizeof(char));
     char* remoteUrl = getReportProfileConfigUrl();
-    T2Info("URL for Report Profile: %s",remoteUrl);
-    T2Debug("%s remoteUrl::%s\n", __FUNCTION__,remoteUrl);
+    T2Info("URL for Report Profile: %s", remoteUrl);
+    T2Debug("%s remoteUrl::%s\n", __FUNCTION__, remoteUrl);
     if ((NULL != urlWithParams) && (NULL != remoteUrl))
     {
         memset(urlWithParams, '0', MAX_URL_LEN * sizeof(char));
@@ -994,16 +999,21 @@ static T2ERROR fetchRemoteReportProfileConfiguration(char **configData) {
         T2Error("Failed to get T2 report profile URL and the required parameters\n");
     }
     if(urlWithParams)
+    {
         free(urlWithParams);
+    }
 
     if(remoteUrl)
+    {
         free(remoteUrl);
+    }
     T2Debug("%s --out\n", __FUNCTION__);
 
     return ret;
 }
 
-static T2ERROR getRemoteReportProfile() {
+static T2ERROR getRemoteReportProfile()
+{
 
     T2Debug("%s ++in\n", __FUNCTION__);
     T2ERROR ret = T2ERROR_FAILURE;
@@ -1013,14 +1023,14 @@ static T2ERROR getRemoteReportProfile() {
     struct timespec _now;
     bool fetchReportProfile = true;
     int returnCode;
-    bool rprofiletypes=false;
+    bool rprofiletypes = false;
 
     while(fetchReportProfile)
     {
         ret = fetchRemoteReportProfileConfiguration(&configData);
         if (ret == T2ERROR_SUCCESS)
         {
-            T2Debug("%s Report Profile Data: %s\n", __FUNCTION__,configData);
+            T2Debug("%s Report Profile Data: %s\n", __FUNCTION__, configData);
             cJSON *reportProfiles = NULL;
             reportProfiles = cJSON_Parse(configData);
             if (reportProfiles)
@@ -1029,7 +1039,8 @@ static T2ERROR getRemoteReportProfile() {
                 cJSON_Delete(reportProfiles);
                 reportProfiles = NULL;
             }
-            if(configData != NULL) {
+            if(configData != NULL)
+            {
                 free(configData);
                 configData = NULL;
             }
@@ -1040,16 +1051,19 @@ static T2ERROR getRemoteReportProfile() {
             T2Warning("XConf Telemetry report profile not set for this device.\n");
             T2Debug("Empty report profiles in configuration. Delete all active profiles. \n");
             clearPersistenceFolder(REPORTPROFILES_PERSISTENCE_PATH);
-            if(configData != NULL) {
+            if(configData != NULL)
+            {
                 free(configData);
                 configData = NULL;
             }
             break;
         }
-        else {
+        else
+        {
             T2Error("Failed to get report profiles from xconf");
 
-            if(configData != NULL) {
+            if(configData != NULL)
+            {
                 free(configData);
                 configData = NULL;
             }
@@ -1073,7 +1087,7 @@ static T2ERROR getRemoteReportProfile() {
             }
             else if (returnCode == 0)
             {
-                T2Error("%s XConfClient Interrupted\n",__FUNCTION__);
+                T2Error("%s XConfClient Interrupted\n", __FUNCTION__);
                 fetchReportProfile = false;
             }
             else
@@ -1210,7 +1224,7 @@ static void* getUpdatedConfigurationThread(void *data)
     char *configData = NULL;
 #if defined(ENABLE_REMOTE_PROFILE_DOWNLOAD)
     char *t2Version = NULL;
-#endif  
+#endif
     pthread_mutex_lock(&xcThreadMutex);
     stopFetchRemoteConfiguration = false ;
     do
@@ -1371,12 +1385,16 @@ static void* getUpdatedConfigurationThread(void *data)
             configURL = NULL;
         }
 #if defined(ENABLE_REMOTE_PROFILE_DOWNLOAD)
-        if(T2ERROR_SUCCESS == getParameterValue(T2_VERSION_DATAMODEL_PARAM, &t2Version) && strcmp(t2Version, "2") !=0) {
+        if(T2ERROR_SUCCESS == getParameterValue(T2_VERSION_DATAMODEL_PARAM, &t2Version) && strcmp(t2Version, "2") != 0)
+        {
             T2Debug("T2 Version = %s\n", t2Version);
-            if(T2ERROR_SUCCESS == getRemoteReportProfile()) {
-                    T2Info("Successfully downloaded report profiles\n");
-            } else {
-                    T2Error("Unable to download the report profile\n");
+            if(T2ERROR_SUCCESS == getRemoteReportProfile())
+            {
+                T2Info("Successfully downloaded report profiles\n");
+            }
+            else
+            {
+                T2Error("Unable to download the report profile\n");
             }
         }
 #endif

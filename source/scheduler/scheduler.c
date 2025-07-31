0afac15366d767c55a6c3689e6a28f9aab29d4a8
Michael Amal Anand <Michael_AmalAnand@comcast.com>
2025-07-31 14:47:48 +0000

    XER10-1921 : Timer times out when there is NTP sync / system clock shifts
    Reason for change:  Timer times out when there is NTP sync / system clock shifts
    Test Procedure: build procedure
    Risks: Medium
    Priority: P2
    
    Signed-off-by: Michael_AmalAnand@comcast.com

--- source/scheduler/scheduler.c
+++ source/scheduler/scheduler.c
@@ -164,17 +164,42 @@ void* TimeoutThread(void *arg)
     registerTriggerConditionConsumer();
     T2Debug("TimeoutThread id is %d\n", (int)tProfile->tId);
 
+    // 1. Initialize condition variable attributes
+    pthread_condattr_t Profile_attr;
+    if (pthread_condattr_init(&Profile_attr) != 0) {
+        perror("pthread_condattr_init failed");
+        return NULL;
+    }
+
     while(tProfile->repeat && !tProfile->terminated && tProfile->name)
     {
         memset(&_ts, 0, sizeof(struct timespec));
         memset(&_now, 0, sizeof(struct timespec));
+
+        //Set the clock source for the condition variable as CLOCK_MONOTONIC
+       // This is important to prevent timer from drifting because of systemtime drift ( such as NTP sync)
+       if (pthread_condattr_setclock(&Profile_attr, CLOCK_MONOTONIC) != 0) {
+           perror("pthread_condattr_setclock failed");
+           pthread_condattr_destroy(&Profile_attr);
+           return NULL;
+       }
+
+       //Initialize the condition variable with the attributes
+       if (pthread_cond_init(&tProfile->tCond, &Profile_attr) != 0) {
+          perror("pthread_cond_init failed");
+          pthread_condattr_destroy(&Profile_attr);
+          return NULL;
+       }
+
+       pthread_condattr_destroy(&Profile_attr);
+
         if(pthread_mutex_lock(&tProfile->tMutex) != 0)
         {
             T2Error("tProfile Mutex lock failed\n");
             return NULL;
         }
 
-        clock_gettime(CLOCK_REALTIME, &_now);
+        clock_gettime(CLOCK_MONOTONIC, &_now);
         //update the timevalues for profiles
         _ts.tv_sec = _now.tv_sec;
 

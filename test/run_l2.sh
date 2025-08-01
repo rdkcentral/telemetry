065169cc76c89ed9ac40fb387a64109b5a767c66
Yogeswaran K <yogeswaransky@gmail.com>
2025-08-01 15:06:22 +0000

    RDK-57625: [RDK-E] Increase L2 Test Coverage For Telemetry
    Signed-off-by: Yogeswaran K <yogeswaransky@gmail.com>

--- test/run_l2.sh
+++ test/run_l2.sh
@@ -34,7 +34,7 @@ fi
 gcc test/functional-tests/tests/app.c -o test/functional-tests/tests/t2_app -ltelemetry_msgsender -lt2utils
 
 # removing --exitfirst flag as it is causing the test to exit after first failure
-#pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/runs_as_daemon.json test/functional-tests/tests/test_runs_as_daemon.py 
-#pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/bootup_sequence.json test/functional-tests/tests/test_bootup_sequence.py 
+pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/runs_as_daemon.json test/functional-tests/tests/test_runs_as_daemon.py 
+pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/bootup_sequence.json test/functional-tests/tests/test_bootup_sequence.py 
 pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/xconf_communications.json test/functional-tests/tests/test_xconf_communications.py --exitfirst
 pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/msg_packet.json test/functional-tests/tests/test_multiprofile_msgpacket.py --exitfirst 

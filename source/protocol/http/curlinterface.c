0336ce65b7bdff6f91c378e6d9a514f3f322886e
adrojaankur <adrojaankur20@gmail.com>
2025-08-19 19:16:01 +0530

    Update curlinterface.c

--- source/protocol/http/curlinterface.c
+++ source/protocol/http/curlinterface.c
@@ -515,13 +515,14 @@ T2ERROR sendReportOverHTTP(char *httpUrl, char *payload, pid_t* outForkedPid)
                         if(curl_code != CURLE_OK || http_code != 200)
                         {
 #ifdef LIBRDKCERTSEL_BUILD
-                            T2Info("%s: Using xpki Certs connection certname: %s\n", __func__, pCertFile);
+                            T2Info("%s: Failed to establish connection using xPKI certificate: %s\n", __func__, pCertFile);
 #endif
                             fprintf(stderr, "curl failed: %s\n", curl_easy_strerror(curl_code));
                             childCurlResponse.lineNumber = __LINE__;
                         }
                         else
                         {
+                            T2Info("%s: Using xpki Certs connection certname: %s\n", __func__, pCertFile);
                             childCurlResponse.lineNumber = __LINE__;
                         }
                         childCurlResponse.curlResponse = curl_code;

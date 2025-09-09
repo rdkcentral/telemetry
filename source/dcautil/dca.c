6e0ad01ade830102e8e4e06225d96b3eb2105423
PriyaDharshini_Kathiravan <priyakathiravan05@gmail.com>
2025-09-09 11:23:33 +0000

    Update dca.c eliminate the algorithm

--- source/dcautil/dca.c
+++ source/dcautil/dca.c
@@ -101,18 +101,23 @@ static const char *strnstr(const char *haystack, const char *needle, size_t len)
     }
 
     // Check minimum length requirements for optimized search
-    if (needle_len < 4)
+    if (needle_len > 0)
     {
         // Use simple search for short patterns
-        for (size_t i = 0; i <= len - needle_len; i++)
+        for (size_t i = 0; i + needle_len <= len; i++)
         {
+            if (haystack[i] == '\0')
+            {
+                break;
+            }
             if (memcmp(haystack + i, needle, needle_len) == 0)
             {
                 return haystack + i;
             }
         }
-        return NULL;
+
     }
+    return NULL;
     /*
         // Adjust search length for longer patterns
         size_t search_len = len - needle_len + 1;
@@ -172,7 +177,7 @@ static const char *strnstr(const char *haystack, const char *needle, size_t len)
                 }
             }
         }
-        */
+
     size_t skip[256];
     for (size_t i = 0; i < 256; ++i)
     {
@@ -198,7 +203,7 @@ static const char *strnstr(const char *haystack, const char *needle, size_t len)
         size_t s = skip[(unsigned char)haystack[i + needle_len - 1]];
         i += (s > 0) ? s : 1;
     }
-    return NULL;
+    return NULL;*/
 }
 
 cJSON *SEARCH_RESULT_JSON = NULL, *ROOT_JSON = NULL;

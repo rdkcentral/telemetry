30f015a7d2ef4e06236553314906bc7a25be6fe2
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-10-18 19:09:04 +0530

    Update build_inside_container.sh

--- build_inside_container.sh
+++ build_inside_container.sh
@@ -29,6 +29,6 @@ DEBUG_CFLAGS="-Wall -Werror -Wextra"
 
 export CFLAGS=" ${DEBUG_CFLAGS} -I${INSTALL_DIR}/include/rtmessage -I${INSTALL_DIR}/include/msgpack -I${INSTALL_DIR}/include/rbus -I${INSTALL_DIR}/include -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/local/include -DFEATURE_SUPPORT_WEBCONFIG -DRDK_LOGGER -DPERSIST_LOG_MON_REF -DDCMAGENT" 
 
-export LDFLAGS="-L/usr/lib/x86_64-linux-gnu -lglib-2.0 -L/usr/local/lib -lrdkloggers"
+export LDFLAGS="-L/usr/lib/x86_64-linux-gnu -lglib-2.0"
 
-./configure --prefix=${INSTALL_DIR} && make && make install
+./configure --prefix=${INSTALL_DIR} LDFLAGS="-L/usr/local/lib -lrdkloggers" && make && make install

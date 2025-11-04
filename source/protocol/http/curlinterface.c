85cbac637a16a060772abc1d29f677a84b228a37
adrojaankur <adrojaankur20@gmail.com>
2025-11-04 18:04:05 +0530

    Remove logs line

--- source/protocol/http/curlinterface.c
+++ source/protocol/http/curlinterface.c
@@ -505,11 +505,6 @@ T2ERROR sendReportOverHTTP(char *httpUrl, char *payload, pid_t* outForkedPid)
                             fprintf(stderr, "curl failed: %s\n", curl_easy_strerror(curl_code));
                             childCurlResponse.lineNumber = __LINE__;
                         }
-                        else
-                        {
-                            T2Info("%s: Using xpki Certs connection certname: %s\n", __func__, pCertFile);
-                            childCurlResponse.lineNumber = __LINE__;
-                        }
                         childCurlResponse.curlResponse = curl_code;
                         childCurlResponse.http_code = http_code;
 

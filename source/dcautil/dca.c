6544a0322897cbcb7d9beb9a87ea613801b01778
PriyaDharshini_Kathiravan <priyakathiravan05@gmail.com>
2025-11-19 10:46:13 +0000

    RDKB-62477:Clients related split markers with value as 0 after Upgraded to 8.3p2s1
    Reason for change: To avoid getting the split markers with zero values in the
    dashboards
    Test Procedure: There should not be crash and regression issues and
    split markers with zero values shouldn't be observed in the report
    Risks: Medium
    Priority: P0
    Signed-off-by: PriyaDharshini_Kathiravan <priyakathiravan05@gmail.com>

--- source/dcautil/dca.c
+++ source/dcautil/dca.c
@@ -614,8 +614,20 @@ static int getAbsolutePatternMatch(FileDescriptor* fileDescriptor, GrepMarker* m
     }
     memcpy(result, start, length);
     result[length] = '\0';
+    //TODO Remove the logic to limit the markers with value as 0 RDKB-62477
+    splitSuffix = strstr(marker->markerName, SPLITMARKER_SUFFIX);
+    if(splitSuffix != NULL && strcmp(splitSuffix, SPLITMARKER_SUFFIX) == 0)
+    {
+        if(strcmp(result, "0") == 0) {
+            marker->u.markerValue = NULL;
+            free(result);
+            result = NULL;
+        }
+        else{
+            marker->u.markerValue = result;
+        }
+    }
 
-    marker->u.markerValue = result;
     T2Debug("%s --out\n", __FUNCTION__);
     return 0;
 }

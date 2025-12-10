5b299227b06226237a1ca1597d7a7422aa217638
Yogeswaran K <yogeswaran_k@comcast.com>
2025-12-10 14:18:54 +0000

    RDKEMW-11275: L1 testcase updation
    Signed-off-by: Yogeswaran K <yogeswaran_k@comcast.com>

--- test/functional-tests/tests/report_profiles.py
+++ test/functional-tests/tests/report_profiles.py
@@ -1303,14 +1303,14 @@ data_temp_with_split_markers = '''{
             "marker": "SYS_INFO_WhoAmI",
             "search": "WhoAmI feature is",
             "logFile": "telemetry2_0.txt.0",
-            "use": "absolute"
+            "use": "accumulate"
           },
           {
             "type": "grep",
             "marker": "SYS_INFO_WhoAmI_Status",
             "search": "WhoAmI feature",
             "logFile": "telemetry2_0.txt.0",
-            "use": "absolute"
+            "use": "accumulate"
           },
           {
             "type": "grep",

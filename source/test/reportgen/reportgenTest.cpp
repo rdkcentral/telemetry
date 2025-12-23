2a751d185a8c8e7ca3682aa0bd91ac667f46b17e
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-12-23 19:02:55 +0530

    added unit test cases

--- source/test/reportgen/reportgenTest.cpp
+++ source/test/reportgen/reportgenTest.cpp
@@ -513,63 +513,6 @@ TEST_F(reportgenTestFixture, encodeGrepResultInJSON4)
     cJSON_Delete(valArray);
 }
 
-TEST_F(reportgenTestFixture, encodeGrepResultInJSON_AccumulateBranchWithRegex)
-{
-    Vector* grepList = nullptr;
-    ASSERT_EQ(Vector_Create(&grepList), T2ERROR_SUCCESS);
-
-    GrepMarker* marker = (GrepMarker*)malloc(sizeof(GrepMarker));
-    memset(marker, 0, sizeof(GrepMarker));
-    marker->markerName = strdup("MyGrep");
-    marker->mType = MTYPE_ACCUMULATE;
-    marker->trimParam = false;
-    marker->regexParam = strdup("[A-Z]+");
-
-    Vector_Create(&marker->u.accumulatedValues);
-    Vector_PushBack(marker->u.accumulatedValues, strdup("FOOBAR"));
-    Vector_PushBack(marker->u.accumulatedValues, strdup("1234"));
-    Vector_PushBack(marker->u.accumulatedValues, strdup("maximum accumulation reached"));
-
-    Vector_PushBack(grepList, marker);
-
-    // Use a "fake" pointer for valArray, just like the mocked cJSON does
-    cJSON* valArray = (cJSON*)0xdeadbeef;
-    cJSON* mockArrayObj = (cJSON*)0x1234;
-    cJSON* mockVectorArray = (cJSON*)0x4321;
-
-    EXPECT_CALL(*m_reportgenMock, cJSON_CreateObject()).Times(1)
-        .WillOnce(::testing::Return(mockArrayObj));
-    EXPECT_CALL(*m_reportgenMock, cJSON_CreateArray()).Times(1)
-        .WillOnce(::testing::Return(mockVectorArray));
-    EXPECT_CALL(*m_reportgenMock, cJSON_CreateString(_)).Times(3)
-        .WillRepeatedly([](const char* str) { return (cJSON*)str; });
-    EXPECT_CALL(*m_reportgenMock, cJSON_AddItemToArray(_, _)).Times(3)
-        .WillRepeatedly(Return(true));
-    EXPECT_CALL(*m_reportgenMock, cJSON_AddItemToObject(mockArrayObj, _, mockVectorArray)).Times(1)
-        .WillOnce(Return(true));
-    EXPECT_CALL(*m_reportgenMock, cJSON_AddItemToArray(valArray, mockArrayObj)).Times(1)
-        .WillOnce(Return(true));
-    EXPECT_CALL(*m_reportgenMock, regcomp(_, _, _)).Times(1)
-        .WillOnce(Return(0));
-    EXPECT_CALL(*m_reportgenMock, regexec(_, _, _, _, _)).Times(3)
-        .WillOnce(Return(0))
-        .WillOnce(Return(1))
-        .WillOnce(Return(0));
-    EXPECT_CALL(*m_reportgenMock, regfree(_)).Times(1);
-
-    EXPECT_EQ(T2ERROR_SUCCESS, encodeGrepResultInJSON(valArray, grepList));
-
-    // No cJSON_Delete or free(valArray)
-    for(size_t i = 0; i < Vector_Size(marker->u.accumulatedValues); ++i) {
-        free(Vector_At(marker->u.accumulatedValues, i));
-    }
-    void FreeString(void* p) { free(p); }
-    Vector_Destroy(marker->u.accumulatedValues, nullptr);
-    free(marker->markerName);
-    free(marker->regexParam);
-    free(marker);
-    Vector_Destroy(grepList, nullptr);
-}
 //When ParamValueCount is 0
 TEST_F(reportgenTestFixture, encodeParamResultInJSON)
 {

b2a4b32d2500321e7a683ab3dcd4ccff4012838b
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2026-01-08 01:03:25 +0530

    test

--- source/test/bulkdata/profileTest.cpp
+++ source/test/bulkdata/profileTest.cpp
@@ -226,9 +226,13 @@ TEST_F(ProfileTest, FreeProfile_Valid) {
 
     // JSON object
     prof->jsonReportObj = cJSON_CreateObject();
-    
-    //add here
+   
+    addProfile(prof);
+
+    enableProfile("profile_name");
+
     getProfileFunc func = getProfileFuncCallback();
+    ASSERT_NE(freeFunc, nullptr);
     T2ERROR err = func("profile_name", &prof);
     EXPECT_EQ(err, T2ERROR_SUCCESS);
 

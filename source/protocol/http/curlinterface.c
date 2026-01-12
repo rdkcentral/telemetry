53a28f92fc67c8d19a111d0b7aa4a55a341eb2ca
adrojaankur <adrojaankur20@gmail.com>
2026-01-12 10:03:34 +0530

    Log certificate path length in curlinterface.c

--- source/protocol/http/curlinterface.c
+++ source/protocol/http/curlinterface.c
@@ -471,6 +471,7 @@ T2ERROR sendReportOverHTTP(char *httpUrl, char *payload, pid_t* outForkedPid)
                     // skip past file scheme in URI
                     telemetry_print_sha256("Telmetry hash", (unsigned char *)pCertPC,strlen(pCertPC));
                     pCertFile = pCertURI;
+                    T2Info("%s:  Certs path: %s len : %d\n", __func__, pCertFile , strlen(pCertPC));
                     if ( strncmp( pCertFile, FILESCHEME, sizeof(FILESCHEME) - 1 ) == 0 )
                     {
                         pCertFile += (sizeof(FILESCHEME) - 1);

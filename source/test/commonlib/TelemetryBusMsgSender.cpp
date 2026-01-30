a3036db116ce6390588e7c001c623e9af0fabf10
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2026-01-30 15:04:32 +0530

    test t2Ready

--- source/test/commonlib/TelemetryBusMsgSender.cpp
+++ source/test/commonlib/TelemetryBusMsgSender.cpp
@@ -283,7 +283,7 @@ TEST_F(TelemetryBusmessageSenderTest, t2_event_f_iscachingenabled_false)
     ret = t2_event_f("marker", 123.456);
     EXPECT_EQ(ret, T2ERROR_SUCCESS);
 }
-#if 0
+#if 1
 TEST_F(TelemetryBusmessageSenderTest, t2_event_d_iscachingenabled_true)
 {
     t2_init((char*)"sysint");
@@ -303,7 +303,7 @@ TEST_F(TelemetryBusmessageSenderTest, t2_event_d_iscachingenabled_true)
 }
 TEST_F(TelemetryBusmessageSenderTest, t2_event_d_iscachingenabled_true_1)
 {
-    t2_init((char*)"telemetry_client");
+    t2_init((char*)"sysint");
 
     EXPECT_CALL(*g_systemMock, access(_,_))
         .WillRepeatedly(Return(-1)); // Accept any number of calls

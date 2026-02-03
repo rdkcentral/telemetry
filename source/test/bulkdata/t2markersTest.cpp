c889143d9db5c95ce5a801a902619cb177c00eca
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2026-02-03 16:32:04 +0530

    cover the lines of T2ER_Uninit()

--- source/test/bulkdata/t2markersTest.cpp
+++ source/test/bulkdata/t2markersTest.cpp
@@ -181,9 +181,16 @@ TEST_F(t2markersTestFixture,  getMarkerProfileList_success)
 
 //t2eventreceiver.c
 
+#ifdef GTEST_ENABLE
+extern "C"
+{
+   void test_set_stopDispatchThread(bool value);
+}
+#endif
 //Uninit is ignored when event receiver is not initialized
 TEST_F(t2markersTestFixture, T2ER_Uninit_ignored_when_not_initialized)
 {
+   test_set_stopDispatchThread(false); 	
    T2ER_Uninit();
 }
 

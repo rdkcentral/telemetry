170f0f6731040f3fcd8a51e7b17c9f6f4cea2c80
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2026-02-12 16:51:57 +0530

    setProfilexconf

--- source/test/bulkdata/profileTest.cpp
+++ source/test/bulkdata/profileTest.cpp
@@ -1380,7 +1380,6 @@ TEST_F(ProfileTest, initReportProfiles) {
         .WillRepeatedly(Return(RBUS_ENABLED));
     
     EXPECT_EQ(initReportProfiles(), T2ERROR_SUCCESS);
-    EXPECT_EQ(ReportProfiles_uninit(),T2ERROR_SUCCESS); //just a trial
 }
 
 TEST_F(ProfileTest, ReportProfiles_addReportProfile) {

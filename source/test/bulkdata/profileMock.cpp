30a8c0e52b4007f82c9f75cc30f53c2b268a0518
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2026-02-18 14:07:25 +0530

    debugged mock error

--- source/test/bulkdata/profileMock.cpp
+++ source/test/bulkdata/profileMock.cpp
@@ -44,7 +44,7 @@ extern "C" T2ERROR __wrap_sendCachedReportsOverHTTP(char *httpUrl, Vector *repor
 }
 
 extern "C" msgpack_unpack_return __wrap_msgpack_unpack_next(msgpack_unpacked *result, const char *data, size_t len, size_t *off) {
-    if (g_profileMockMock) return g_profileMock->msgpack_unpack_next(result, data, len, off);
+    if (g_profileMock) return g_profileMock->msgpack_unpack_next(result, data, len, off);
     // Real or stub implementation if not mocked
     return MSGPACK_UNPACK_SUCCESS;
 }

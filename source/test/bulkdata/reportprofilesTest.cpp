8b3acac0c563787b6c0678996e135a931a631cc0
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2026-02-18 16:07:06 +0530

    debug __ReportProfiles_ProcessReportProfilesMsgPackBlob

--- source/test/bulkdata/reportprofilesTest.cpp
+++ source/test/bulkdata/reportprofilesTest.cpp
@@ -169,6 +169,10 @@ TEST_F(reportprofilesTestFixture, ProcessMsgPackBlob_ProfileAdded) {
 
     EXPECT_CALL(*g_reportprofileMock, isRbusEnabled())
      .WillRepeatedly(Return(false));
+
+     EXPECT_CALL(*g_vectorMock, Vector_Size(_))
+        .Times(::testing::AtMost(2))
+        .WillRepeatedly(Return(0));
     // Mock success for config parsing and addition
     //EXPECT_CALL(*profileMock, processMsgPackConfiguration(_, _)).WillOnce(Return(T2ERROR_SUCCESS));
     //EXPECT_CALL(*profileMock, ReportProfiles_addReportProfile(_)).WillOnce(Return(T2ERROR_SUCCESS));
@@ -292,16 +296,13 @@ TEST_F(reportprofilesTestFixture, ProcessMsgPackBlob_Test2) {
    struct __msgpack__ *msg = (struct __msgpack__*)malloc(sizeof(struct __msgpack__));
    msg->msgpack_blob = (char*)webConfigString;
    msg->msgpack_blob_size = (int)decodedDataLen;
-#if 0
+#if 1
        EXPECT_CALL(*g_vectorMock, Vector_Size(_))
         .Times(::testing::AtMost(1))
         .WillRepeatedly(Return(0)); // Return 1 to indicate only one profile (no duplicates)
     EXPECT_CALL(*g_vectorMock, Vector_Destroy(_, _)).Times(::testing::AtMost(1))
         .WillRepeatedly(Return(T2ERROR_SUCCESS));
 #endif
-     EXPECT_CALL(*g_vectorMock, Vector_Size(_))
-        .Times(::testing::AtMost(2))
-        .WillRepeatedly(Return(0));
 // call target
   int ret = ReportProfiles_ProcessReportProfilesMsgPackBlob((void*)msg, false);
 //  EXPECT_EQ(ret, T2ERROR_PROFILE_NOT_FOUND);

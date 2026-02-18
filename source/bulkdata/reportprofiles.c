49dae33db281acc2e531d82652696ca51a82668b
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2026-02-18 14:17:03 +0530

    debugged mock error

--- source/bulkdata/reportprofiles.c
+++ source/bulkdata/reportprofiles.c
@@ -88,9 +88,6 @@
 static cap_user appcaps;
 #endif
 
-#ifdef GTEST_ENABLE
-#define msgpack_unpack_next  __wrap_msgpack_unpack_next 
-#endif
 
 static BulkData bulkdata;
 static bool rpInitialized = false;

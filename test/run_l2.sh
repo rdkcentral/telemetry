6aab5c44d09501480fc888feccda3fbaa5c22f47
Yogeswaran K <yogeswaransky@gmail.com>
2026-02-22 17:35:32 +0000

    RDK-60476: Update L1 and L2 testcases for fork elimination
    Signed-off-by: Yogeswaran K <yogeswaransky@gmail.com>

--- test/run_l2.sh
+++ test/run_l2.sh
@@ -33,8 +33,15 @@ fi
 
 gcc test/functional-tests/tests/app.c -o test/functional-tests/tests/t2_app -ltelemetry_msgsender -lt2utils
 
+final_result=0
 # removing --exitfirst flag as it is causing the test to exit after first failure
-pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/runs_as_daemon.json test/functional-tests/tests/test_runs_as_daemon.py 
-pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/bootup_sequence.json test/functional-tests/tests/test_bootup_sequence.py 
-pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/xconf_communications.json test/functional-tests/tests/test_xconf_communications.py
-pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/msg_packet.json test/functional-tests/tests/test_multiprofile_msgpacket.py
+pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/runs_as_daemon.json test/functional-tests/tests/test_runs_as_daemon.py || final_result=1
+pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/bootup_sequence.json test/functional-tests/tests/test_bootup_sequence.py || final_result=1
+pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/xconf_communications.json test/functional-tests/tests/test_xconf_communications.py || final_result=1
+pytest -v --json-report --json-report-summary --json-report-file $RESULT_DIR/msg_packet.json test/functional-tests/tests/test_multiprofile_msgpacket.py || final_result=1
+
+if [ $final_result -ne 0 ]; then
+    echo "Some tests failed. Please check the JSON reports in $RESULT_DIR for details."
+else
+    echo "All tests passed successfully."
+fi

98b9d76caf646c8e220ff1c3159f6adc62f09054
Yogeswaran K <yogeswaransky@gmail.com>
2026-04-15 17:48:56 +0000

    RDKEMW-16851: Fix for skipfrequency issue in top markers
    Signed-off-by: Yogeswaran K <yogeswaransky@gmail.com>

--- source/dcautil/dca.c
+++ source/dcautil/dca.c
@@ -319,6 +319,17 @@ int processTopPattern(char* profileName,  Vector* topMarkerList, int profileExec
             continue;
         }
 
+        if (topMarkerObj->cpuValue)
+        {
+            free(topMarkerObj->cpuValue);
+            topMarkerObj->cpuValue = NULL;
+        }
+        if (topMarkerObj->memValue)
+        {
+            free(topMarkerObj->memValue);
+            topMarkerObj->memValue = NULL;
+        }
+
         // If the skip frequency is set, skip the marker processing for this interval
         int tmp_skip_interval, is_skip_param;
         tmp_skip_interval = topMarkerObj->skipFreq;

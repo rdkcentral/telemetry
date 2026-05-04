a53e6c8679d745fc598998b885adb6e9b145097e
onkar-panchare1 <onkar.panchare@telekom-digital.com>
2026-05-04 22:30:19 +0530

    TELEMETRY-2: Dynamic telemetry markers for Dynamic Tables in Telemetry (#161)
    Description:
    Introduced marker type DataModelTable for dynamic tables in telemetry.
    
    Enhanced T2 agent to resolve and collect data from all instances of multi-instance object tables when dynamic markers are configured.
    
    Added logic to parse DataModelTable, filter markers based on telemetry profile configuration, and encode them in telemetry reports after collection, ensuring telemetry reports include per-instance data for configured dynamic markers.
    
    Added support for an "index" parameter in configuration to restrict data collection to specific indexes if required.
    
    Reason for change:
    Enable telemetry to capture dynamic, per-instance data from tables (e.g., Host table, Wi-Fi associated devices) where indexes are not fixed, ensuring richer and more flexible telemetry collection.
    
    Signed-off-by: onkar.panchare1 <onkar.panchare@telekom-digital.com>
    Co-authored-by: Shibu Kakkoth Vayalambron <shibu.kakkoth@gmail.com>

--- source/bulkdata/profile.c
+++ source/bulkdata/profile.c
@@ -31,6 +31,7 @@
 #include "t2markers.h"
 #include "t2log_wrapper.h"
 #include "busInterface.h"
+#include "ccspinterface.h"
 #include "curlinterface.h"
 #include "rbusmethodinterface.h"
 #include "scheduler.h"
@@ -201,6 +202,10 @@ static void freeProfile(void *data)
         {
             Vector_Destroy(profile->cachedReportList, free);
             profile->cachedReportList = NULL;
+        }
+        if(profile->dataModelTableList)
+        {
+            Vector_Destroy(profile->dataModelTableList, freeDataModelTable);
         }
         if(profile->jsonReportObj)
         {
@@ -504,7 +509,14 @@ static void* CollectAndReport(void* data)
                     profileParamVals = getProfileParameterValues(profile->paramList, count);
                     if(profileParamVals != NULL)
                     {
-                        encodeParamResultInJSON(valArray, profile->paramList, profileParamVals);
+                        if (Vector_Size(profile->dataModelTableList) > 0)
+                        {
+                            encodeParamResultInJSON(valArray, profile->paramList, profileParamVals, profile->dataModelTableList);
+                        }
+                        else
+                        {
+                            encodeParamResultInJSON(valArray, profile->paramList, profileParamVals, NULL);
+                        }
                     }
                     Vector_Destroy(profileParamVals, freeProfileValues);
                 }

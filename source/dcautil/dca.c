9253634f1258f15aee0d6299d3f36d1e2899808a
Yogeswaran K <166126056+yogeswaransky@users.noreply.github.com>
2026-07-08 15:16:29 +0530

    RDKEMW-16851: [Cherry-pick #332] Fix for skipfrequency issue in top markers (#332) (#391)
    * RDKEMW-16851: Fix for skipfrequency issue in top markers
    
    
    
    * RDKEMW-16851: Fix for skipfrequency issue in top markers
    
    
    
    * RDKEMW-16851: Fix for skipfrequency issue in top markers
    
    
    
    * RDKEMW-16851: Fix for skipfrequency issue in top markers
    
    
    
    * RDKEMW-16851: Fix for skipfrequency issue in top markers
    
    
    
    * RDKEMW-16851: Fix for skipfrequency issue in top markers
    
    
    
    * RDKEMW-16851: Fix for skipfrequency issue in top markers
    
    
    
    * RDKEMW-16851: Fix for skipfrequency issue in top markers
    
    
    
    * RDKEMW-16851: Fix for skipfrequency issue in top markers
    
    
    
    ---------
    
    Signed-off-by: Yogeswaran K <yogeswaransky@gmail.com>

--- source/dcautil/dca.c
+++ source/dcautil/dca.c
@@ -275,6 +275,16 @@ int processTopPattern(char* profileName,  Vector* topMarkerList, int profileExec
         {
             continue;
         }
+        if (topMarkerObj->cpuValue)
+        {
+            free(topMarkerObj->cpuValue);
+            topMarkerObj->cpuValue = NULL;
+        }
+        if (topMarkerObj->memValue)
+        {
+            free(topMarkerObj->memValue);
+            topMarkerObj->memValue = NULL;
+        }
         int tmp_skip_interval, is_skip_param;
         tmp_skip_interval = topMarkerObj->skipFreq;
         if(tmp_skip_interval <= 0)
@@ -310,6 +320,16 @@ int processTopPattern(char* profileName,  Vector* topMarkerList, int profileExec
             continue;
         }
 
+        if (topMarkerObj->cpuValue)
+        {
+            free(topMarkerObj->cpuValue);
+            topMarkerObj->cpuValue = NULL;
+        }
+        if (topMarkerObj->memValue)
+        {
+            free(topMarkerObj->memValue);
+            topMarkerObj->memValue = NULL;
+        }
 
         // If the skip frequency is set, skip the marker processing for this interval
         int tmp_skip_interval, is_skip_param;
